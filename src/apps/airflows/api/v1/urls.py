ad640a4906e0afc926b86bbe4d27c55a6fa5e7cb
macbook <macbook@apple.com>
2022-11-12 20:11:49 +0600

    corrected api

--- src/apps/airflows/api/v1/urls.py
+++ src/apps/airflows/api/v1/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from apps.airflows.api.v1.views.airflow_search_views import AirflowSearchCreateAPIView
 from apps.airflows.api.v1.views.provider_views import ProviderAListAPIView, ProviderBListAPIView
 
 urlpatterns = [
     path("provider-a/search/", ProviderAListAPIView.as_view(), name="provider-a"),
     path("provider-b/search/", ProviderBListAPIView.as_view(), name="provider-b"),
+    path("airflow/search/", AirflowSearchCreateAPIView.as_view(), name="airflow-search-create"),
+    path("airflow/<uuid:pk>/<str:currency>/", ProviderBListAPIView.as_view(), name="airflow-search-retrieve"),
 ]
