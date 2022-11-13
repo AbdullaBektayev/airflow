15c2145ffffac7ccd97278eea96c7cf157087cb6
macbook <macbook@apple.com>
2022-11-13 21:33:43 +0600

    corrected data serach

---
+++ src/fixtures/mocked_request_fixtures.py
@@ -0,0 +1,25 @@
+import pytest
+
+PROVIDER_BASE_URL = "http://localhost:9000/api/v1/flights/"
+
+
+@pytest.fixture()
+def mocked_provider_a_search(requests_mock, json_data_by_path):
+    response_data = json_data_by_path("./src/fixtures/data/search_a.json")
+    url = PROVIDER_BASE_URL + "/provider-a/search/"
+    requests_mock.post(
+        url=url,
+        json=response_data,
+        status_code=200,
+    )
+
+
+@pytest.fixture()
+def mocked_provider_b_search(requests_mock, json_data_by_path):
+    response_data = json_data_by_path("./src/fixtures/data/search_b.json")
+    url = PROVIDER_BASE_URL + "/provider-b/search/"
+    requests_mock.post(
+        url=url,
+        json=response_data,
+        status_code=200,
+    )
