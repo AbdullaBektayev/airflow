3029392f92c887ce6624ecc5f24866984f07e769
macbook <macbook@apple.com>
2022-11-14 00:47:33 +0600

    corrected tasks

--- src/apps/flights/tasks/update_currency_tasks.py
+++ src/apps/flights/tasks/update_currency_tasks.py
@@ -1,19 +1,6 @@
-from typing import List
-
 from src import celery_app
-from src.apps.flights.models import Currency
 from src.apps.flights.services.ariflow_services import AirflowService
 from src.apps.flights.services.currency_services import CurrencyUpdater
-
-
-def get_update_currency(response_dict) -> List[Currency]:
-    currency_list = []
-    for item in response_dict["rates"]["item"]:
-        currency_obj, created = Currency.objects.update_or_create(
-            title=item["title"], defaults={"fullname": item["fullname"], "in_kzt": item["description"]}
-        )
-        currency_list.append(currency_obj)
-    return currency_list
 
 
 @celery_app.task(
--- src/settings/contrib/celery.py
+++ src/settings/contrib/celery.py
@@ -1,6 +1,8 @@
+from celery.schedules import crontab
+from celery.signals import worker_ready
+
 from ..django import TIME_ZONE as DJANGO_TIME_ZONE
 from ..environment import env
-
 
 CELERY_TASK_ALWAYS_EAGER = env.bool("SRC_CELERY_TASK_ALWAYS_EAGER", default=False)
 CELERY_BROKER_URL = env.str("SRC_CELERY_BROKER", default="redis://redis:6379/1")
@@ -9,4 +11,12 @@ CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = DJANGO_TIME_ZONE
 
-CELERYBEAT_SCHEDULE = {}
+CELERYBEAT_SCHEDULE = {
+    "update_currency_task": {"task": "flights.update_currency_task", "schedule": crontab(minute=00, hour=12)},
+}
+
+
+@worker_ready.connect
+def updated_currency_at_start(sender, **kwargs):
+    with sender.app.connection() as conn:
+        sender.app.send_task('flights.update_currency_task', connection=conn)
